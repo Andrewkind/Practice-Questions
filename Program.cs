34632eaf3a29f56f06a9a96a8f46177f69faa746
Andrew Kind <andrew.w.kind@gmail.com>
2020-06-25 23:39:22 -0600

    Add practice problem 2.

--- Program.cs
+++ Program.cs
@@ -10,10 +10,15 @@ namespace Practice_Questions
     class Program
     {
 
+
+        // Write the Main Menu Lines
         static void writeLines()
         {
 
-            Console.WriteLine("Pratice Problems");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" Pratice Problems \n");
+            Console.BackgroundColor = ConsoleColor.Black;
+
             Console.WriteLine("Select 1-4");
             Console.WriteLine("1- int to power of 3");
             Console.WriteLine("2- divide diners bill (double division)");
@@ -21,6 +26,8 @@ namespace Practice_Questions
             Console.WriteLine("4- sum of digits");
         }
 
+        // Validate user input from main menu
+        // It should be a key from numbers 1-4
         static bool validateInput(string userInput)
         {
             int numberOfQuestions = 4;
@@ -39,7 +46,7 @@ namespace Practice_Questions
                 }
 
             }
-            newLine();
+            Console.WriteLine("\n");
 
             Console.WriteLine("Invalid input: {0}", userInput);
             return false;
@@ -48,6 +55,7 @@ namespace Practice_Questions
 
 
 
+        //Perform tasks to prompt the main menu
         static void promptMainMenu()
         {
             Console.Clear();
@@ -69,6 +77,7 @@ namespace Practice_Questions
             //  Console.WriteLine("userInput: {0}", userInput);
 
 
+            Console.Clear();
             switch (userInput)
             {
 
@@ -76,7 +85,7 @@ namespace Practice_Questions
                     runPractice1();
                     break;
                 case "2":
-
+                    runPractice2();
                     break;
                 default:
 
@@ -89,21 +98,21 @@ namespace Practice_Questions
             promptMainMenu();
         }
 
-        static void newLine()
-        {
-            Console.WriteLine("\n");
+        // Helper method for new line
 
-        }
 
+
+        // Practice Problem 1
+
+        //Write a program that prompts the user for a number (as an int) and outputs the cube of that number (raised to the third power).
 
         static void runPractice1()
         {
 
-            //Write a program that prompts the user for a number (as an int) and outputs the cube of that number (raised to the third power).
-            newLine();
-            Console.WriteLine("Practice Problem 1: Get a integer and cube it.");
-
-            Console.WriteLine("Please enter a number.");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" Practice Problem 1: Get an integer and cube it. ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("\nPlease enter a number.");
 
             int number;
             string userInput;
@@ -119,16 +128,64 @@ namespace Practice_Questions
 
             Console.WriteLine("{0} cubed is {1}.", number.ToString(), cubed.ToString());
 
-            promptEnter();
+            promptAnyKey();
             promptMainMenu();
 
         }
 
-        static void promptEnter()
+        //Write a program that will take in a total dollar value (as a double) and a number of diners (as an int) and calculate the amount of the bill that each diner should pay (equal split).
+        static void runPractice2()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("Practice Problem 2: Split the bill. Get a double and divide.");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+
+            // Get the user input for the bill amount.
+            // Must be a double value
+            Console.WriteLine("\nWhat was the total on the bill?");
+
+            double cost;
+            string userInput;
+            while (!Double.TryParse(userInput = Console.ReadLine(), out cost))
+            {
+                Console.WriteLine("Invalid input. {0} is not a double.", userInput);
+
+            }
+
+            // Get the number of guests.
+            // Guest number is an int
+            Console.WriteLine("How many guests to split by?");
+
+            int guests;
+            while (!Int32.TryParse(userInput = Console.ReadLine(), out guests))
+            {
+                Console.WriteLine("Invalid input. {0} is not a int.", userInput);
+
+            }
+            ////
+            ////
+
+            // Calculate cost per guest. 
+            double costPerGuest = cost / guests;
+
+            // Round answer
+            costPerGuest = Math.Round(costPerGuest, 2);
+
+
+            //https://stackoverflow.com/questions/23361554/number-to-string-always-2-decimal-place
+            Console.WriteLine("With the total value being {0}$ and {1} guests, the split bill would be {2}$ per guest.", cost.ToString("0.00"), guests.ToString(), costPerGuest.ToString("0.00"));
+
+            promptAnyKey();
+            promptMainMenu();
+        }
+
+        static void promptAnyKey()
         {
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
-            newLine();
+            Console.WriteLine("\n");
 
         }
     }
